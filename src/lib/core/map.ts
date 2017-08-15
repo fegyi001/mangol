d50c2369dd1e547f4e37cc09d86e8437eaf2bb37
fegyi001 <padanyigulyasgergely@lltk.local>
2017-08-15 08:06:18 +0200

    upgrade Angular, angular-cli and OpenLayers

--- src/lib/core/map.ts
+++ src/lib/core/map.ts
@@ -1,52 +1,53 @@
-import { MangolLayer, MangolLayergroup } from './../core/_index';
 
 import * as ol from 'openlayers';
+import { MangolLayer } from './layer';
+import { MangolLayergroup } from './layergroup';
 
 export class MangolMap extends ol.Map {
 
-    options: any;
+  options: any;
 
-    layers: MangolLayer[];
-    layerGroups: MangolLayergroup[];
+  layers: MangolLayer[];
+  layerGroups: MangolLayergroup[];
 
-    constructor(options: any) {
-        super(options);
-        this.options = options;
-        this.layers = [];
-        this.layerGroups = [];
+  constructor(options: any) {
+    super(options);
+    this.options = options;
+    this.layers = [];
+    this.layerGroups = [];
+  }
+
+  public addLayersAndLayerGroups(optionLayers: any[]): any {
+    for (let i = 0; i < optionLayers.length; i++) {
+      const element = optionLayers[i];
+      this.handleLayerOrLayerGroup(element, null);
     }
+  }
 
-    public addLayersAndLayerGroups(optionLayers: any[]): any {
-        for (let i = 0; i < optionLayers.length; i++) {
-            const element = optionLayers[i];
-            this.handleLayerOrLayerGroup(element, null);
-        }
+  private handleLayerOrLayerGroup(element: any, layerGroup: MangolLayergroup): any {
+    if (element.type === 'layer') {
+      const newLayer = new MangolLayer(element);
+      this.addLayer(element.layer);
+      if (layerGroup !== null) {
+        layerGroup.getChildren().push(newLayer);
+      } else {
+        this.layers.push(newLayer);
+      }
+    } else if (element.type === 'layergroup') {
+      const newLayerGroup = new MangolLayergroup(element);
+      this.layerGroups.push(newLayerGroup);
+      for (let i = 0; i < element.children.length; i++) {
+        this.handleLayerOrLayerGroup(element.children[i], newLayerGroup);
+      }
     }
+  }
 
-    private handleLayerOrLayerGroup(element: any, layerGroup: MangolLayergroup): any {
-        if (element.type === 'layer') {
-            const newLayer = new MangolLayer(element);
-            this.addLayer(element.layer);
-            if (layerGroup !== null) {
-                layerGroup.getChildren().push(newLayer);
-            } else {
-                this.layers.push(newLayer);
-            }
-        } else if (element.type === 'layergroup') {
-            const newLayerGroup = new MangolLayergroup(element);
-            this.layerGroups.push(newLayerGroup);
-            for (let i = 0; i < element.children.length; i++) {
-                this.handleLayerOrLayerGroup(element.children[i], newLayerGroup);
-            }
-        }
-    }
+  public getMangolLayers(): MangolLayer[] {
+    return this.layers;
+  }
 
-    public getMangolLayers(): MangolLayer[] {
-        return this.layers;
-    }
-
-    public getMangolLayerGroups(): MangolLayergroup[] {
-        return this.layerGroups;
-    }
+  public getMangolLayerGroups(): MangolLayergroup[] {
+    return this.layerGroups;
+  }
 
 }
