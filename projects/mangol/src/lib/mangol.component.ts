d7f34eba78771dc6e170da5e954d189d136b8cec
Gergely Padányi-Gulyás <fegyi@Gergelys-MacBook-Pro.local>
2018-06-09 21:57:36 +0200

    basic material sidenav

--- projects/mangol/src/lib/mangol.component.ts
+++ projects/mangol/src/lib/mangol.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mangol',
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MangolComponent implements OnInit {
   @Input() config: any;
-
+  showFiller = false;
   constructor() {}
 
   ngOnInit() {}
--- projects/mangol/src/lib/mangol.module.ts
+++ projects/mangol/src/lib/mangol.module.ts
@@ -1,11 +1,22 @@
+import 'hammerjs';
+
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MangolComponent } from './mangol.component';
 
 @NgModule({
-  imports: [BrowserModule, CommonModule],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    MatButtonModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
+  ],
   declarations: [MangolComponent],
   exports: [MangolComponent]
 })
