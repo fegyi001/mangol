946fd3897dba65cf44dd7501f4f22856c0871396
Gergely Padányi-Gulyás <fegyi001@gmail.com>
2018-06-25 12:15:50 +0200

    material tree start

--- projects/mangol/src/lib/modules/layertree/layertree.component.ts
+++ projects/mangol/src/lib/modules/layertree/layertree.component.ts
@@ -1,15 +1,135 @@
-import { Component, OnInit } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { BehaviorSubject, of as observableOf } from 'rxjs';
+
+/**
+ * Json node data with nested structure. Each node has a filename and a value or a list of children
+ */
+export class FileNode {
+  children: FileNode[];
+  filename: string;
+  type: any;
+}
+
+/**
+ * The Json tree data in string. The data could be parsed into Json object
+ */
+const TREE_DATA = `
+  {
+    "Documents": {
+      "angular": {
+        "src": {
+          "core": "ts",
+          "compiler": "ts"
+        }
+      },
+      "material2": {
+        "src": {
+          "button": "ts",
+          "checkbox": "ts",
+          "input": "ts"
+        }
+      }
+    },
+    "Downloads": {
+        "Tutorial": "html",
+        "November": "pdf",
+        "October": "pdf"
+    },
+    "Pictures": {
+        "Sun": "png",
+        "Woods": "jpg",
+        "Photo Booth Library": {
+          "Contents": "dir",
+          "Pictures": "dir"
+        }
+    },
+    "Applications": {
+        "Chrome": "app",
+        "Calendar": "app",
+        "Webstorm": "app"
+    }
+  }`;
+/**
+ * File database, it can build a tree structured Json object from string.
+ * Each node in Json object represents a file or a directory. For a file, it has filename and type.
+ * For a directory, it has filename and children (a list of files or directories).
+ * The input will be a json object string, and the output is a list of `FileNode` with nested
+ * structure.
+ */
+@Injectable()
+export class FileDatabase {
+  dataChange: BehaviorSubject<FileNode[]> = new BehaviorSubject<FileNode[]>([]);
+
+  get data(): FileNode[] {
+    return this.dataChange.value;
+  }
+
+  constructor() {
+    this.initialize();
+  }
+
+  initialize() {
+    // Parse the string to json object.
+    const dataObject = JSON.parse(TREE_DATA);
+
+    // Build the tree nodes from Json object. The result is a list of `FileNode` with nested
+    //     file node as children.
+    const data = this.buildFileTree(dataObject, 0);
+
+    // Notify the change.
+    this.dataChange.next(data);
+  }
+
+  /**
+   * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
+   * The return value is the list of `FileNode`.
+   */
+  buildFileTree(value: any, level: number): FileNode[] {
+    let data: any[] = [];
+    for (let k in value) {
+      let v = value[k];
+      let node = new FileNode();
+      node.filename = `${k}`;
+      if (v === null || v === undefined) {
+        // no action
+      } else if (typeof v === 'object') {
+        node.children = this.buildFileTree(v, level + 1);
+      } else {
+        node.type = v;
+      }
+      data.push(node);
+    }
+    return data;
+  }
+}
 
 @Component({
   selector: 'mangol-layertree',
   templateUrl: './layertree.component.html',
-  styleUrls: ['./layertree.component.scss']
+  styleUrls: ['./layertree.component.scss'],
+  providers: [FileDatabase]
 })
 export class LayertreeComponent implements OnInit {
+  nestedTreeControl: NestedTreeControl<FileNode>;
 
-  constructor() { }
+  nestedDataSource: MatTreeNestedDataSource<FileNode>;
 
-  ngOnInit() {
+  constructor(database: FileDatabase) {
+    this.nestedTreeControl = new NestedTreeControl<FileNode>(this._getChildren);
+    this.nestedDataSource = new MatTreeNestedDataSource();
+
+    database.dataChange.subscribe(data => (this.nestedDataSource.data = data));
   }
 
+  ngOnInit() {}
+
+  private _getChildren = (node: FileNode) => {
+    return observableOf(node.children);
+  };
+
+  hasNestedChild = (_: number, nodeData: FileNode) => {
+    return !nodeData.type;
+  };
 }
--- projects/mangol/src/lib/modules/layertree/layertree.module.ts
+++ projects/mangol/src/lib/modules/layertree/layertree.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+
 import { LayertreeComponent } from './layertree.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatTreeModule, MatIconModule, MatButtonModule],
   declarations: [LayertreeComponent],
   exports: [LayertreeComponent]
 })
