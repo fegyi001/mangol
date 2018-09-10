5093a3d46e21ba43cef7a5850d33e87934a89a3b
Padányi-Gulyás Gergely <padanyi-gulyasg@si.net>
2018-09-10 11:40:11 +0200

    demo api

--- src/app/etc/api/api.component.ts
+++ src/app/etc/api/api.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-api',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api.component.scss']
 })
 export class ApiComponent implements OnInit {
+  @Input()
+  apiUrl: string;
+  apiFullUrl: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.apiFullUrl = `https://fegyi001.github.io/mangol/modules/${
+      this.apiUrl
+    }.html`;
   }
 
+  openAPIPage() {
+    window.open(this.apiFullUrl);
+  }
 }
